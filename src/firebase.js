fca4b289a5069b9467776c8229bee84d296e5ffb
AbdulAzeez Bello <abdulazeezbello5882@gmail.com>
2025-11-23 01:12:36 +0100

    Env Create

--- src/firebase.js
+++ src/firebase.js
@@ -3,17 +3,17 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB2nC8DTax1nHlhlH8WvOqcQclO4a6lF_A",
-  authDomain: "admitsextra-waitlist.firebaseapp.com",
-  databaseURL: "https://admitsextra-waitlist-default-rtdb.firebaseio.com",
-  projectId: "admitsextra-waitlist",
-  storageBucket: "admitsextra-waitlist.firebasestorage.app",
-  messagingSenderId: "552263956236",
-  appId: "1:552263956236:web:cc8de0447d7465ee46fb29",
-  measurementId: "G-7LD57QWGJF"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 
-export { database }; // Only export database
+export { database };

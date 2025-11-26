7d2a91badb069cc92aa84275f6e64eab22a4a175
AbdulAzeez Bello <abdulazeezbello5882@gmail.com>
2025-11-26 21:40:39 +0100

    Updated Mail Host

--- api/sendMail.js
+++ api/sendMail.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
   if (!email) return res.status(400).send("Email required");
 
   const transporter = nodemailer.createTransport({
-    host: "mail.admitsxtra.com",
+    host: "smtp.hostinger.com",
     port: 465,
     secure: true,
     auth: {
